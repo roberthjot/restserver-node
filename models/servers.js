4e744c22ddc0111791d04ade2c1667dad79c257f
Roberth <roberthjot@gmail.com>
2023-05-11 21:59:54 -0400

    mongodb

--- models/servers.js
+++ models/servers.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 
+const { dbConnection } = require('../database/config')
+
 class Server {
 
     constructor() {
@@ -8,11 +10,18 @@ class Server {
         this.port = process.env.PORT
         this.userPatch = '/api/users'
 
+        // Conectar a base de datos
+        this.conectarDB()
+
         // Middlewares
         this.middlewares()
 
         // Rutas de mi app
         this.routes();
+    }
+
+    async conectarDB() {
+        await dbConnection()
     }
 
     middlewares() {
